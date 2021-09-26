f8c533af5186df468ce4dcb38116b49a9c280344
Jason Ko <jekb2019@gmail.com>
2021-09-26 14:26:30 +1300

    Jason

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+  return <div className="App">hello Dhwani</div>;
 }
 
 export default App;
